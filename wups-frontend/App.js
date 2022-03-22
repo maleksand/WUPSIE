41c2eadcb93c90fb95468988d903a20476caf7ae
1011454MBJ <76095482+1011454MBJ@users.noreply.github.com>
2022-03-22 13:59:00 +0100

    Implemented menu bar
    Along with some work in progress

--- wups-frontend/App.js
+++ wups-frontend/App.js
@@ -1,21 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { render } from 'react-dom';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+//import Dash from './dashboard.js';
+import Indexed from './index.js';
 
-export default function App() {
+export default class App extends React.Component{
+  render(){
+    <link rel='stylesheet' href='./node_modules/react-grid-layout/css/styles.css'></link>
+    //var dashboard = new DashBoard();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View style={styles.menubar}>
+    {/* <View style={styles.container}> */}
+      {/* <View style={styles.menubar}> */}
+        {/* <DashBoard /> */}
+        <Text style={{color: "white"}}>Menu bar</Text>
+        {/* <Text>Open up App.js to start working on your app!</Text> */}
+        {/* <StatusBar style="auto" /> */}
+      </View>
+    // </View>
+    // </View>
   );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  menubar:{
+    height: 70,
+    minWidth: 150,
+    maxWidth: Dimensions.get('window').width,
+    backgroundColor: '#000000',
+    justifyContent: 'center',
+    alignItems: 'center',
+    //textColor:{color: '#ffffff'},
+  },
+
 });
--- wups-frontend/index.js
+++ wups-frontend/index.js
@@ -1,8 +1,34 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
+import {Text, View, AppRegistry} from 'react-native';
+import React from 'react';
+
+export default class Indexed extends React.Component {
+
+    render() {
+        <link rel='stylesheet' href='./node_modules/react-grid-layout/css/styles.css'></link>
+        //<link rel='stylesheet' href='./node_modules/react-resizable/css/styles.css'></link>
+           
+        return(
+           //<ScrollView style={styles.scrollContainer}>
+             //<View style={styles.container}>
+                // <View style={styles.box}>
+               <View>
+                 {/* <div>
+                   <link rel="stylesheet" href="//cdn.jsdelivr.net/chartist.js/latest/chartist.min.css"></link>
+                   <ChartistGraph data={data} options={options} type={type} />
+                 </div> */}
+                 <Text>Box 1</Text>
+               </View>
+        )
+    }
+}
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+
+AppRegistry.registerComponent('Indexed', () => Indexed);
+AppRegistry.runApplication('Indexed', {rootTag: document.getElementById('Indexed')});
