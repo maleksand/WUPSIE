709ac1be6ea0ed6ebd3e587fa7390f1a6f6f1ac8
aleksandra <79192662+maleksand@users.noreply.github.com>
2022-03-25 10:23:50 +0100

    sacrificed my soul to make the buttons uglier

--- wups-frontend/App.js
+++ wups-frontend/App.js
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { render } from 'react-dom';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, Modal } from 'react-native';
+import { Button } from 'react-native'
+
 //import Dash from './dashboard.js';
 import Indexed from './index.js';
 import Menubar from './Menubar.js';
@@ -28,9 +30,22 @@ export default class App extends React.Component{
 
       </View>
       <View style={styles.buttonBar}>
-        <Text style={{color:'yellow'}}>This where our buttons go</Text>
 
+
+      
+
+      <View style={styles.buttoncontainer} >
+      <Button title="help " color="orange" style={styles.button}> </Button>   
+      <Button title="me " color="pink" style={styles.button} > </Button>
+      <Button title="I'm" color="blue" style={styles.button}> </Button>
+      <Button title=" in" color="green" style={styles.button}> </Button>
+      <Button title=" react" color="yellow" style={styles.button}> </Button>
+      
+
+      <Button title="prison" color="purple"> </Button>
       </View>
+
+    </View>
       <View styles={styles.container}>
         <View styles={styles.graphBox}>
           
@@ -55,7 +70,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     flexWrap: 'wrap',
     backgroundColor: '#fff',
-    //alignItems: 'center',
+    alignItems: 'center',
     //justifyContent: 'center',
     //padding:2,
   },
@@ -64,8 +79,7 @@ const styles = StyleSheet.create({
     minWidth: 150,
     maxWidth: Dimensions.get('window').width,
     backgroundColor: '#000000',
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'space-evenly',
     //textColor:{color: '#ffffff'},
   },
   buttonBar:{
@@ -73,8 +87,10 @@ const styles = StyleSheet.create({
     //minWidth: 150,
     maxWidth: Dimensions.get('window').width,
     backgroundColor: '#000055',
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent:"space-evenly",
+    alignItems: 'stretch',
+    
+    padding:10,
     //textColor:{color: '#ffffff'},
   },
   graphBox:{
@@ -84,10 +100,39 @@ const styles = StyleSheet.create({
     height: 200,
     //backgroundColor: '#eeeeee',
     backgroundColor: '#ff0000',
-    justifyContent: 'center',
-    alignItems: 'stretch',
+    justifyContent:"space-evenly",
+    // alignItems: 'stretch',
     //padding: 30,
 
   },
 
+ 
+
+ buttoncontainer: {
+  height: 50,
+  //minWidth: 150,
+  maxWidth: Dimensions.get('window').width,
+  backgroundColor: '#000055',
+    flex: 1,
+    flexDirection: 'row',
+    // alignItems: 'center',
+    justifyContent:"space-evenly",
+        padding:10,
+    
+  },
+  space: {
+    justifyContent:"space-evenly",
+
+    flex: 1,
+    width: 20,
+    height: 20,
+  },
+  button: {
+    justifyContent:"space-evenly",
+    width: '40%',
+
+    flex: 1,
+    padding: 10
+  },
+  
 });
