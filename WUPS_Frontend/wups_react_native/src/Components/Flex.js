8c28e50466ae8b6b3320ac879d40448eb5b03961
Rasmus Emil Bøgel Larsen <1086243@ucn.dk>
2022-03-30 09:52:23 +0200

    Added example of how to define styles in a seperate file in React

--- WUPS_Frontend/wups_react_native/src/Components/Flex.js
+++ WUPS_Frontend/wups_react_native/src/Components/Flex.js
@@ -1,32 +1,34 @@
 import React from "react";
 // import Styles from '../App.css'
 import { StyleSheet, Text, View } from "react-native";
-import Beers from './Beers';
+// import Beers from './Beers';
+import styles from "../styles"
 
 
 
 const Flex = () => {
     return (
-        <View style={[styles.container, { flexDirection: "row" }]}>
+        <View style={[styles.container1, { flexDirection: "row" }]}>
 
-            <View style={{ flex: 2, backgroundColor: "red" }}>
+            <View style={{ flex: 1, backgroundColor: "red" }}>
                 <Text>This is where a graph will go</Text>
             </View>
-            <View style={{ flex: 2, backgroundColor: "darkorange" }} >
+            <View style={{ flex: 1, backgroundColor: "darkorange" }} >
                 <Text>This is where graph 2 will go</Text>
 
             </View>
-            <View style={{ flex: 2, backgroundColor: "green" }} >
-                <Beers/>
+            <View style={{ flex: 1, backgroundColor: "green" }} >
+                <Text>This is where graph 3 will go</Text>
             </View>
 
         </View>
     )
 }
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 20,
-    },
-});
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         padding: 20,
+//         width: "100%"
+//     },
+// });
 export default Flex;
---
+++ WUPS_Frontend/wups_react_native/src/styles.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { StyleSheet } from "react-native"
+
+let styles = {}
+
+
+// These is just as an example for now
+styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20
+    },
+})
+
+styles = StyleSheet.create({
+    container2: {
+        flex: 1,
+        padding: 20,
+        width: "100%"
+    },
+})
+
+export default styles
