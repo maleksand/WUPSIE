ac92f46b2c9fb3bacb9a2a4c18d44f207ceb22ff
1011454MBJ <76095482+1011454MBJ@users.noreply.github.com>
2022-04-07 13:47:34 +0200

    Have a button

--- WUPS_Frontend/wups-pwa/src/components/Overview.js
+++ WUPS_Frontend/wups-pwa/src/components/Overview.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../App.css';
+import GraphPlaceholder from './Graphs/CommonGraphElement/GraphPlaceholder';
 
 const Overview = () => {
     
     return(
         <div className='Overview-container'>
-            Graphs go here
+            <GraphPlaceholder />
         </div>
 
     )
