18b75b1d236aa1ffa3aea40d24b9591450c395c8
Rasmus Emil Bøgel Larsen <1086243@ucn.dk>
2022-04-07 10:00:47 +0200

    header is present

---
+++ WUPS_Frontend/wups-pwa/src/components/Header/Header.js
@@ -0,0 +1,36 @@
+import '../../App.css';
+import { Link } from 'react-router-dom';
+import { Dropdown } from './Dropdown';
+
+
+
+
+function Nav() {
+  const navStyle = {
+    color: 'white'
+  };
+  return (
+    <nav>
+      <Link style={navStyle} to='/'>
+        <h3>Logo</h3>
+      </Link>
+      This is WUPS IOT overview
+      {/* Add a dropdown here */}
+
+      <Dropdown>
+        <Link style={navStyle} to='/about'>
+          <li>About</li>
+        </Link>
+        <Link style={navStyle} to='/profile'>
+          <li>Profile</li>
+        </Link>
+        <Link style={navStyle} to='/beers'>
+          <li>Beers(ForTestApi)</li>
+        </Link>
+      </Dropdown>
+
+    </nav>
+  );
+}
+
+export default Nav;
