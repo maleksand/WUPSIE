83eeb7aee85616607dd66446680f4b5b1aed9396
1011454MBJ <76095482+1011454MBJ@users.noreply.github.com>
2022-04-07 10:48:54 +0200

    Router technical works
    Needs styling

--- WUPS_Frontend/wups-pwa/src/App.js
+++ WUPS_Frontend/wups-pwa/src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
+import NotFound from './components/NotFound';
+import About from './components/About';
+import Overview from './components/Overview';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
-        {/* <Routes>
+        <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/beers' element={<Beers />} />
-          <Route path="/beer/:id"element={<BeerDetails/>} />
+          {/* <Route path='/profile' element={<Profile />} /> */}
+          {/* <Route path='/beers' element={<Beers />} /> */}
+          {/* <Route path="/beer/:id"element={<BeerDetails/>} /> */}
           <Route path="*" element={<NotFound/>}/>
-        </Routes> */}
+        </Routes>
       </div>
     </Router>
   );
 }
+
+const Home = () => (
+  <div>
+    {/* <h1>Under construction</h1> */}
+    <Overview />
+  </div>
+)
 
 export default App;
---
+++ WUPS_Frontend/wups-pwa/src/components/Overview.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+const Overview = () => {
+    
+    return(
+        <div className='Overview-container'>
+            Graphs go here
+        </div>
+
+    )
+}
+
+export default Overview;
