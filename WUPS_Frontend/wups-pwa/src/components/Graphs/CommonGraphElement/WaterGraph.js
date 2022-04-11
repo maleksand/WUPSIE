c1a6d49479dd5a0c52e22c66ba9168cecfba17db
1011454MBJ <76095482+1011454MBJ@users.noreply.github.com>
2022-04-11 22:00:32 +0200

    Made graph render with proper ticks

--- WUPS_Frontend/wups-pwa/src/components/Graphs/CommonGraphElement/WaterGraph.js
+++ WUPS_Frontend/wups-pwa/src/components/Graphs/CommonGraphElement/WaterGraph.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../../logo.svg';
+//import logo from '../../../logo.svg';
 import '../../../App.css';
 import { BarChart, CartesianGrid, XAxis, YAxis, Bar, Tooltip, Label, Legend } from 'recharts';
 
@@ -17,24 +17,40 @@ const WaterGraph = (prop) => {
 
     }
     return (
-        <div className='graph-button'>
-        {/* <button id={prop.id} onClick={onPressHandler} className='graph-button'> */}
-             {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <div className='graph-button' >
 
-            <BarChart height={400} width={600} data={jsonData} onClick={onPressHandler} margin={{ top: 15, right: 10, left: 15, bottom: 5 }}>
+
+            <BarChart height={400} width={600} data={jsonData} onClick={onPressHandler} margin={{ top: 15, right: 10, left: 15, bottom: 30 }}>
                 <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="timestamp" >
-                    <Label value={'Hot Water Graph'} />
-                </XAxis>
+                <XAxis 
+                    dataKey='timestamp.$date'
+                    angle={90} 
+                    interval={0} 
+                    scaleToFit={true} 
+                    textAnchor={'inherit'} 
+                    fontSize={12}
+
+                    tickFormatter={(tick) => {
+                        let unixDate = new Date(tick)
+                        //var unixYear = unixDate.getFullYear.toString.slice(2)
+                        var unixFormatted = ''
+                            + (unixDate.getDay("DD") < 10 ? '0' + unixDate.getDay() : unixDate.getDay()) + '-'
+                            + (unixDate.getMonth("MM") < 10 ? '0' + unixDate.getMonth() : unixDate.getMonth()) + ' '
+                            //+ unixYear + ' ' 
+                            + (unixDate.getHours("hh") < 10 ? '0' + unixDate.getHours() : unixDate.getHours()) + ':'
+                            + (unixDate.getMinutes('mm') < 10 ? '0' + unixDate.getMinutes() : unixDate.getMinutes())
+                        //console.log(unixFormatted)
+                        return unixFormatted;
+                    }} />
+
                 <YAxis type="number" domain={['auto', 'auto']}>
                     <Label value={'Cubic meters'} angle={-90} position={'left'} />
                 </YAxis>
                 <Tooltip />
-                <Legend />
+                <Legend /*verticalAlign='bottom'*/  wrapperStyle={{position:'bottom'}} />
                 <Bar dataKey="measurement" fill="#8884d8" />
             </BarChart>
 
-        {/* </button> */}
         </div>
     );
 }
