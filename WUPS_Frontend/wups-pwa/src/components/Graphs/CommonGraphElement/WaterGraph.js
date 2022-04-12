cc04f6fca22d1d486d4cd70d61f763a8229a67ea
1011454MBJ <76095482+1011454MBJ@users.noreply.github.com>
2022-04-12 08:45:42 +0200

    Reformatted dateconversion
    And made it more readable

--- WUPS_Frontend/wups-pwa/src/components/Graphs/CommonGraphElement/WaterGraph.js
+++ WUPS_Frontend/wups-pwa/src/components/Graphs/CommonGraphElement/WaterGraph.js
@@ -1,14 +1,10 @@
 import React from 'react';
-//import logo from '../../../logo.svg';
 import '../../../App.css';
 import { BarChart, CartesianGrid, XAxis, YAxis, Bar, Tooltip, Label, Legend } from 'recharts';
 
-//Metertype value from local json-file
-const Meter = "hot water"
-
 //This needs local data
 const jsonData = require('./Data/Data.json');
-console.log(jsonData)
+//console.log(jsonData)
 
 
 const WaterGraph = (prop) => {
@@ -19,8 +15,18 @@ const WaterGraph = (prop) => {
     return (
         <div className='graph-button' >
 
-
-            <BarChart height={400} width={600} data={jsonData} onClick={onPressHandler} margin={{ top: 15, right: 10, left: 15, bottom: 30 }}>
+            <BarChart 
+                height={400}
+                width={600}
+                data={jsonData}
+                onClick={onPressHandler}
+                margin={{ 
+                    top: 15, 
+                    right: 10, 
+                    left: 15, 
+                    bottom: 52 
+                }}
+            >
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis 
                     dataKey='timestamp.$date'
@@ -29,26 +35,29 @@ const WaterGraph = (prop) => {
                     scaleToFit={true} 
                     textAnchor={'inherit'} 
                     fontSize={12}
+                    tickFormatter={(tick) => UnixConversion(tick)
+                    } />
 
-                    tickFormatter={(tick) => {
-                        let unixDate = new Date(tick)
-                        //var unixYear = unixDate.getFullYear.toString.slice(2)
-                        var unixFormatted = ''
-                            + (unixDate.getDay("DD") < 10 ? '0' + unixDate.getDay() : unixDate.getDay()) + '-'
-                            + (unixDate.getMonth("MM") < 10 ? '0' + unixDate.getMonth() : unixDate.getMonth()) + ' '
-                            //+ unixYear + ' ' 
-                            + (unixDate.getHours("hh") < 10 ? '0' + unixDate.getHours() : unixDate.getHours()) + ':'
-                            + (unixDate.getMinutes('mm') < 10 ? '0' + unixDate.getMinutes() : unixDate.getMinutes())
-                        //console.log(unixFormatted)
-                        return unixFormatted;
-                    }} />
-
-                <YAxis type="number" domain={['auto', 'auto']}>
-                    <Label value={'Cubic meters'} angle={-90} position={'left'} />
+                <YAxis 
+                    type="number"
+                    domain={['auto', 'auto']}
+                >
+                    <Label 
+                        value={'Cubic meters'} 
+                        angle={-90} 
+                        position={'left'} 
+                    />
                 </YAxis>
                 <Tooltip />
-                <Legend /*verticalAlign='bottom'*/  wrapperStyle={{position:'bottom'}} />
-                <Bar dataKey="measurement" fill="#8884d8" />
+                <Legend
+                    wrapperStyle={{ position: 'bottom' }}
+                    payload={[{
+                        value: 'Hot water',
+                        type: 'rect',
+                        color: "#167c1f"
+                    }]}
+                />
+                <Bar dataKey="measurement" fill="#167c1f" />
             </BarChart>
 
         </div>
@@ -56,3 +65,15 @@ const WaterGraph = (prop) => {
 }
 
 export default WaterGraph;
+
+function UnixConversion(tick) {
+    let unixDate = new Date(tick)
+    var unixFormatted = ''
+        + (unixDate.getDate("DD") < 10 ? '0' + unixDate.getDate() : unixDate.getDate()) + '-'
+        + (unixDate.getMonth("MM") < 10 ? '0' + unixDate.getMonth() : unixDate.getMonth()) + '-'
+        + unixDate.getFullYear() + ' '
+        + (unixDate.getHours("hh") < 10 ? '0' + unixDate.getHours() : unixDate.getHours()) + ':'
+        + (unixDate.getMinutes('mm') < 10 ? '0' + unixDate.getMinutes() : unixDate.getMinutes())
+    return unixFormatted;
+
+}
