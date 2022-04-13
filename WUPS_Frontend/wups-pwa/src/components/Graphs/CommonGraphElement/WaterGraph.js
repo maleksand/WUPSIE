826b90b25ff07ad10598e02e5e4598acdc411d83
PHahnUCN <1081439@ucn.dk>
2022-04-13 14:55:16 +0200

    fixed a NON COMPILING main merge

--- WUPS_Frontend/wups-pwa/src/components/Graphs/CommonGraphElement/WaterGraph.js
+++ WUPS_Frontend/wups-pwa/src/components/Graphs/CommonGraphElement/WaterGraph.js
@@ -10,15 +10,15 @@ const WaterGraph = () => {
     function onPressHandler() {
         window.alert('Routing in progress')
 
-     }
+    }
 
 
     const [data, setData] = useState([]);
 
     useEffect(() => {
         // Must take timespan to make it more universal
-        var startDate='2019-03-01 00:00:00';
-        var endDate='2019-03-01 23:59:59';
+        var startDate = '2019-03-01 00:00:00';
+        var endDate = '2019-03-01 23:59:59';
 
         async function getData() {
             let json = await FetchAPI(startDate, endDate)
@@ -30,72 +30,68 @@ const WaterGraph = () => {
 
     }, [])
 
-    useEffect(() => {
-        //  console.log(data)
-
-
-    return (
-        <div className='graph-button' >
-            <NavLink to="/notFound">
-                <BarChart
-                    height={400}
-                    width={600}
-                    data={data}
-                    //onClick={onPressHandler()}
-                    margin={{
-                        top: 15,
-                        right: 10,
-                        left: 15,
-                        bottom: 52
-                    }}
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis
-                        dataKey='timestamp'
-                        angle={90}
-                        interval={0}
-                        scaleToFit={true}
-                        textAnchor={'inherit'}
-                        fontSize={12}
-                        tickFormatter={(tick) => UnixConversion(tick)
-                        } />
-
-                    <YAxis
-                        type="number"
-                        domain={['auto', 'auto']}
+        return (
+            <div className='graph-button' >
+                <NavLink to="/notFound">
+                    <BarChart
+                        height={400}
+                        width={600}
+                        data={data}
+                        //onClick={onPressHandler()}
+                        margin={{
+                            top: 15,
+                            right: 10,
+                            left: 15,
+                            bottom: 52
+                        }}
                     >
-                        <Label
-                            value={'Cubic meters'}
-                            angle={-90}
-                            position={'left'}
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis
+                            dataKey='timestamp'
+                            angle={90}
+                            interval={0}
+                            scaleToFit={true}
+                            textAnchor={'inherit'}
+                            fontSize={12}
+                            tickFormatter={(tick) => UnixConversion(tick)
+                            } />
+
+                        <YAxis
+                            type="number"
+                            domain={['auto', 'auto']}
+                        >
+                            <Label
+                                value={'Cubic meters'}
+                                angle={-90}
+                                position={'left'}
+                            />
+                        </YAxis>
+
+                        <Tooltip />
+                        <Legend
+                            wrapperStyle={{ position: 'bottom' }}
+                            payload={[{
+                                value: 'Hot water', //ToDo Make this dynamic
+                                type: 'rect',
+                                color: "#167c1f"
+                            }]}
                         />
-                    </YAxis>
-                    
-                    <Tooltip />
-                    <Legend
-                        wrapperStyle={{ position: 'bottom' }}
-                        payload={[{
-                            value: 'Hot water', //ToDo Make this dynamic
-                            type: 'rect',
-                            color: "#167c1f"
-                        }]}
-                    />
-                    <Bar dataKey="value" fill="#167c1f" /*onClick={onPressHandler()}*/ />
-                </BarChart>
-            </NavLink>
-        </div>
-    );
-}
+                        <Bar dataKey="value" fill="#167c1f" /*onClick={onPressHandler()}*/ />
+                    </BarChart>
+                </NavLink>
+            </div>
+        );
+    }
 
 export default WaterGraph;
 
-function UnixConversion(tick) {
-    let unixDate = new Date(tick)
-    var unixFormatted = ''
-        + (unixDate.getDate("DD") < 10 ? '0' + unixDate.getDate() : unixDate.getDate()) + '-'
-        + (unixDate.getMonth("MM") < 10 ? '0' + unixDate.getMonth() : unixDate.getMonth()) + '-'
-        + unixDate.getFullYear() + ' '
-        + (unixDate.getHours("hh") < 10 ? '0' + unixDate.getHours() : unixDate.getHours()) + ':'
-        + (unixDate.getMinutes('mm') < 10 ? '0' + unixDate.getMinutes() : unixDate.getMinutes())
-    return unixFormatted;
-}
+    function UnixConversion(tick) {
+        let unixDate = new Date(tick)
+        var unixFormatted = ''
+            + (unixDate.getDate("DD") < 10 ? '0' + unixDate.getDate() : unixDate.getDate()) + '-'
+            + (unixDate.getMonth("MM") < 10 ? '0' + unixDate.getMonth() : unixDate.getMonth()) + '-'
+            + unixDate.getFullYear() + ' '
+            + (unixDate.getHours("hh") < 10 ? '0' + unixDate.getHours() : unixDate.getHours()) + ':'
+            + (unixDate.getMinutes('mm') < 10 ? '0' + unixDate.getMinutes() : unixDate.getMinutes())
+        return unixFormatted;
+    }
