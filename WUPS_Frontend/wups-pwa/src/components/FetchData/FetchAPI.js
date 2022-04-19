4ebd2ba9affc0973eebcf39e78c84b91d0d54e31
Smileyrasmus-UCN <1086243@ucn.dk>
2022-04-19 09:57:55 +0200

    small optimisation in FetchAPI

--- WUPS_Frontend/wups-pwa/src/components/FetchData/FetchAPI.js
+++ WUPS_Frontend/wups-pwa/src/components/FetchData/FetchAPI.js
@@ -3,17 +3,11 @@
 //const url = 'http://localhost:3030/api/devices/A92C3B84F9F4E0AF/measurements?startDate=2019-03-01&endDate=2019-03-02&deviceType=watermeasurement'  //HOT WATER
 
 
-
-async function getData(startDate, endDate) {
+async function FetchAPI(startDate, endDate) {
     const url = 'http://localhost:3030/api/devices/BBB60CC9ED69C910/measurements?startDate=' + startDate + '&endDate=' + endDate + '&deviceType=watermeasurement'
 
     let response = await fetch(url);
     let json = await response.json()
-    return json
-}
-
-async function FetchAPI(startDate, endDate) {
-    let json = await getData(startDate, endDate)
     return json
   }
   
