7788db254c9beced8df48c81f64d165da5b6c469
Smileyrasmus-UCN <1086243@ucn.dk>
2022-04-19 10:22:11 +0200

    Created module

--- WUPS_Frontend/wups-pwa/src/App.js
+++ WUPS_Frontend/wups-pwa/src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import NotFound from './components/NotFound';
 import About from './components/About';
 import Overview from './components/Overview';
+import processData from "./Logic/processDataToSumStructure"
 
 function App() {
+  const [data, setData] = useState({})
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch("http://localhost:3030/api/households/01C21CA24FBCECE7/devices/measurements?startDate=2019-02-01&endDate=2019-08-01")
+      const jsonData = await response.json()
+      setData(processData(jsonData))
+    }
+    getData()
+  }, [])
+
+  useEffect(() => {
+    console.log(data)
+  }, [data])
+
+
+
   return (
     <div className="App">
       <Router>
---
+++ WUPS_Frontend/wups-pwa/src/Logic/processDataToSumStructure.js
@@ -0,0 +1,3 @@
+export default function processData(data) {
+    return data
+}
