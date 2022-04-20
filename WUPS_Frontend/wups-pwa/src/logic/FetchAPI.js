0d043c6bca0885338f9317cf8f94950a0ecbcc8a
PHahnUCN <1081439@ucn.dk>
2022-04-20 13:31:03 +0200

    commit

--- WUPS_Frontend/wups-pwa/src/logic/FetchAPI.js
+++ WUPS_Frontend/wups-pwa/src/logic/FetchAPI.js
@@ -1,14 +1,27 @@
-async function getData() {
-    const url = 'http://localhost:3030/api/devices/BBB60CC9ED69C910/measurements?startDate=2019-03-01&deviceType=watermeasurement'
-    let response = await fetch(url);
-    let json = await response.json()
+let fetchApi = {}
+
+fetchApi.getFromOneDayData = async (deviceId, date) => {
+    async function getData() {
+        const url = `http://localhost:3030/api/devices/${deviceId}/measurements?startDate=${date}&deviceType=watermeasurement`
+        let response = await fetch(url);
+        let json = await response.json()
+        return json
+    }
+
+    let json = await getData()
+    return json
+
+}
+
+fetchApi.getFromDateRange = async (deviceId, startDate, endDate) => {
+    async function getData() {
+        const url = `http://localhost:3030/api/devices/${deviceId}/measurements?startDate=${startDate}&endDate=${endDate}&deviceType=watermeasurement`
+        let response = await fetch(url);
+        let json = await response.json()
+        return json
+    }
+    let json = await getData()
     return json
 }
 
-async function FetchAPI() {
-    let json = await getData()
-    return json
-  }
-  
-  
-  export default FetchAPI;
+export default fetchApi;
