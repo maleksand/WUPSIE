0a4d25e0be58bb5d8ca5d7d0577668d9ff306a47
Smileyrasmus-UCN <1086243@ucn.dk>
2022-04-20 09:54:27 +0200

    Tree structure, now with sums :D

--- WUPS_Frontend/wups-pwa/src/App.js
+++ WUPS_Frontend/wups-pwa/src/App.js
@@ -8,11 +8,21 @@ import Overview from './components/Overview';
 import processData from "./Logic/processDataToSumStructure"
 
 function App() {
+
+  /*
+   * Review start:
+   * Delete this before merge with main
+   */
   const [data, setData] = useState({})
 
   useEffect(() => {
     async function getData() {
-      const response = await fetch("http://localhost:3030/api/households/01C21CA24FBCECE7/devices/measurements?startDate=2019-02-01&endDate=2019-08-01")
+      let e = new Date("2019-01-19 00:00")
+      e.setHours(e.getHours() + e.getTimezoneOffset() / 60)
+      let ee = new Date("2019-02-19 00:00")
+      ee.setHours(ee.getHours() + ee.getTimezoneOffset() / 60)
+
+      const response = await fetch("http://localhost:3030/api/households/01C21CA24FBCECE7/devices/measurements?startDate=" + e.toISOString() + "&endDate=" + ee.toISOString())
       const jsonData = await response.json()
       setData(processData(jsonData))
     }
@@ -22,6 +32,9 @@ function App() {
   useEffect(() => {
     console.log(data)
   }, [data])
+  /**
+   * Review end
+   */
 
 
 
--- WUPS_Frontend/wups-pwa/src/Logic/processDataToSumStructure.js
+++ WUPS_Frontend/wups-pwa/src/Logic/processDataToSumStructure.js
@@ -1,38 +1,84 @@
 function findMinMaxDate(data) {
     let minDate = new Date(8640000000000000) // the maximum date possiple
     let maxDate = new Date(-8640000000000000) // the minimum date possiple
-    for(let device of data) {
-        for(let measurement of device.measurements) {
+    for (let device of data) {
+        for (let measurement of device.measurements) {
             const measurementDate = new Date(measurement.timestamp)
-            if(minDate > measurementDate) minDate = measurementDate
-            if(maxDate < measurementDate) maxDate = measurementDate
+            if (minDate > measurementDate) minDate = measurementDate
+            if (maxDate < measurementDate) maxDate = measurementDate
         }
     }
+    /*
+     * because we are iterating on day to day basis,
+     * setting the time to 00:00:00 ensures all days are acounted for.
+     * In other words, don't delete the next two lines
+     */
+    minDate.setHours(0, 0, 0)
+    maxDate.setHours(0, 0, 0)
     return [minDate, maxDate]
 }
 
-export default function processData(data) {
+function createTree(data) {
     let processedData = {}
     const [minDate, maxDate] = findMinMaxDate(data)
     let date = minDate
-    while(date <= maxDate){
-        for(let device of data) {
-            if(!processedData[device.deviceId])                                      processedData[device.deviceId] = {}
-            if(!processedData[device.deviceId][date.getFullYear()])                  processedData[device.deviceId][date.getFullYear()] = {}
-            if(!processedData[device.deviceId][date.getFullYear()][date.getMonth()]) processedData[device.deviceId][date.getFullYear()][date.getMonth()] = {}
-            processedData[device.deviceId][date.getFullYear()][date.getMonth()][date.getDate()] = {sum: 0, measurements: []}
+    
+    // create the structure
+    while (date <= maxDate) {
+        for (let device of data) {
+            if (!processedData[device.deviceId]) processedData[device.deviceId] = { sum: 0 }
+            if (!processedData[device.deviceId][date.getFullYear()]) processedData[device.deviceId][date.getFullYear()] = { sum: 0 }
+            if (!processedData[device.deviceId][date.getFullYear()][date.getMonth() + 1]) processedData[device.deviceId][date.getFullYear()][date.getMonth() + 1] = { sum: 0 }
+
+            processedData[device.deviceId][date.getFullYear()][date.getMonth() + 1][date.getDate()] = { sum: 0, measurements: [] }
         }
-        date.setDate(date.getDate() + 1)
+        date.setDate(date.getDate() + 1) // this can skip a day, fix it
     }
-    
-    
-    for(let device of data) {
-        for(let measurement of device.measurements) {
+
+    // add all leafs to the branches of the tree
+    for (let device of data) {
+        for (let measurement of device.measurements) {
             const measurementDate = new Date(measurement.timestamp)
-            processedData[device.deviceId][measurementDate.getFullYear()][measurementDate.getMonth()][measurementDate.getDate()]["measurements"].push(measurement)
+            processedData[device.deviceId][measurementDate.getFullYear()][measurementDate.getMonth() + 1][measurementDate.getDate()].measurements.push(measurement)
         }
     }
-    console.log(processedData)
-    
+
+    // calculate the sums for device, year, month and day
+    for (let device in processedData){
+        if(device === "sum") continue
+        for(let year in processedData[device]){
+            if(year === "sum") continue
+            for(let month in processedData[device][year]) {
+                if(month === "sum") continue
+                for(let day in processedData[device][year][month]){
+                    if(day === "sum") continue
+                    for(let measurement of processedData[device][year][month][day].measurements) {
+                        processedData[device][year][month][day].sum += measurement.usage
+                    }
+                    processedData[device][year][month].sum += processedData[device][year][month][day].sum
+                }
+                processedData[device][year].sum += processedData[device][year][month].sum
+            }
+            processedData[device].sum += processedData[device][year].sum
+        }
+    }
+    return processedData
+}
+
+function addUsageToData(data) {
+    for(let device of data) {
+        let lastMeasurementValue
+        for(let measurement of device.measurements) {
+            if(!lastMeasurementValue) lastMeasurementValue = measurement.value
+            measurement.usage = measurement.value - lastMeasurementValue
+            lastMeasurementValue = measurement.value
+        }
+    }
+    return data
+}
+
+export default function processData(data) {
+    const dataWithUsage = addUsageToData(data)
+    const processedData = createTree(dataWithUsage)
     return processedData
 }
