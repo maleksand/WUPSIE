012e8eb9fa7f0cf93aa9584c2a301b059d28078c
PHahnUCN <1081439@ucn.dk>
2022-04-29 13:15:47 +0200

    [ID9] Have created a chart that use props.data from overvies (dbContext creates some rendering bugs) Next task. Make it update dynamically when clicked (years, months, day)

---
+++ WUPS_Frontend/wups-pwa/src/components/Graphs/PieChart.js
@@ -0,0 +1,93 @@
+import "../../App.css"
+import { PieChart, Pie, Cell, LabelList } from "recharts"
+import { useState, useCallback } from "react"
+import { DataContext } from "../../App"
+
+function valueLabelFormatter(label) {
+    return label += " m3"
+}
+
+export default function ExamplePieChart(props) {
+    
+    const [data, setData] = useState(props.data)
+    const [isButtonDisabled, setIsButtonDisabled] = useState(true)
+    
+    const onPressHandler = useCallback(()=>{
+        return console.log("testing pie slice click")
+    }, [])
+    const backHandler = useCallback(() => {
+        return "testing back handler"
+    }, [])
+    
+    return (
+        <div >
+            <h1>This is your "pizza" chart of usage</h1>
+                <button type="button" onClick={backHandler} disabled={isButtonDisabled}>Back</button>
+            <PieChart width={600} height={400}>
+                <Pie
+                    dataKey="usageSum"
+                    data={data}
+                    fill="#8884d8"
+                >
+                    {data.map((entry, index) => (
+                        <Cell key={`cell-${entry.id}`} fill={entry.meterType === "cold water" ? "#6A7FDB" : "#A93F55"} onClick={onPressHandler} />
+                    ))}
+                    <LabelList dataKey="meterType" position="outside" fill="black" stroke="" offset={30}/>
+                    <LabelList dataKey="usageSum" position="inside" fill="white" stroke="" formatter={valueLabelFormatter} onClick={onPressHandler}/>
+                </Pie>
+            </PieChart>
+        </div>
+    )
+}
+const data01 = [
+    {
+      "name": "Group A",
+      "value": 400
+    },
+    {
+      "name": "Group B",
+      "value": 300
+    },
+    {
+      "name": "Group C",
+      "value": 300
+    },
+    {
+      "name": "Group D",
+      "value": 200
+    },
+    {
+      "name": "Group E",
+      "value": 278
+    },
+    {
+      "name": "Group F",
+      "value": 189
+    }
+  ];
+  const data02 = [
+    {
+      "name": "Group A",
+      "value": 2400
+    },
+    {
+      "name": "Group B",
+      "value": 4567
+    },
+    {
+      "name": "Group C",
+      "value": 1398
+    },
+    {
+      "name": "Group D",
+      "value": 9800
+    },
+    {
+      "name": "Group E",
+      "value": 3908
+    },
+    {
+      "name": "Group F",
+      "value": 4800
+    }
+  ];
--- WUPS_Frontend/wups-pwa/src/components/Overview.js
+++ WUPS_Frontend/wups-pwa/src/components/Overview.js
@@ -5,13 +5,14 @@ import WaterGraph from './Graphs/CommonGraphElement/WaterGraph';
 import UsageComponent from './Usage/UsageMainComponent';
 import { Link } from 'react-router-dom';
 import { DataContext } from '../App';
-
-
+import PieChartComponent from './Graphs/PieChart';
+import ExamplePieChart from './Graphs/PieChart';
 const Overview = () => {
     const data = useContext(DataContext)
 
     return (
         <div className='Overview-container'>
+            <ExamplePieChart data={data}/>
             {data.map(device => {
                 return (<WaterGraph key={device.id} device={device} />)
             })
