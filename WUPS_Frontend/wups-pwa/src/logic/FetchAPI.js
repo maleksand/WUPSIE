ea21bb6f12e53a8472b47a1a2702f75157713bb6
Rasmus Emil Bøgel Larsen <1086243@ucn.dk>
2022-04-29 15:38:23 +0200

    refactor fetchApi

--- WUPS_Frontend/wups-pwa/src/logic/FetchAPI.js
+++ WUPS_Frontend/wups-pwa/src/logic/FetchAPI.js
@@ -1,4 +1,6 @@
-let fetchApi = {}
+let fetchApi = {
+    baseUrl: "http://localhost:3030/api"
+}
 
 async function getData(url) {
     let response = await fetch(url);
@@ -17,30 +19,30 @@ async function getData(url) {
 // }
 
 fetchApi.getFromOneDayData = async (deviceId, date) => {
-    const url = `http://localhost:3030/api/devices/${deviceId}/measurements?startDate=${date}&deviceType=watermeasurement`
+    const url = `${fetchApi.baseUrl}/devices/${deviceId}/measurements?startDate=${date}&deviceType=watermeasurement`
     return await getData(url)
 }
 
 fetchApi.getFromDateRange = async (deviceId, startDate, endDate) => {
     //console.log(validateDates([startDate, endDate]))
-    const url = `http://localhost:3030/api/devices/${deviceId}/measurements?startDate=${startDate}&endDate=${endDate}&deviceType=watermeasurement`
+    const url = `${fetchApi.baseUrl}/devices/${deviceId}/measurements?startDate=${startDate}&endDate=${endDate}&deviceType=watermeasurement`
     return await getData(url)
 }
       // http://localhost:3030/api/households/01C21CA24FBCECE7/devices/measurements?startDate=2019-06-01&endDate=2020-06-01
 
 fetchApi.getHouseholdFromDateRange = async (householdId, startDate, endDate) => {
-    const url = `http://localhost:3030/api/households/${householdId}/devices/measurements?startDate=${startDate}&endDate=${endDate}`
+    const url = `${fetchApi.baseUrl}/households/${householdId}/devices/measurements?startDate=${startDate}&endDate=${endDate}`
     return await getData(url)
 }
 
 fetchApi.getUserHouseholdsDevices = async (userId, startDate, endDate) => {
-    const households = await getData(`http://localhost:3030/api/users/${userId}/households`)
+    const households = await getData(`${fetchApi.baseUrl}/users/${userId}/households`)
     const householdId = households[0].id
     return fetchApi.getHouseholdFromDateRange(householdId, startDate, endDate)
 }
 
 fetchApi.getRegionPrice = async (regionId) =>{
-    const url = `http://localhost:3030/api/regions/${regionId}/price`
+    const url = `${fetchApi.baseUrl}/regions/${regionId}/price`
     const regionData = await getData(url)
     return regionData.pricePerCubic
 }
